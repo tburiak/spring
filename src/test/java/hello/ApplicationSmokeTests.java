8c7379ce2cf33e6eb88f27e1091cc9f2ecdbc1a9
tbur <tarasburyak@gmail.com>
2019-05-01 22:48:37 +0300

    Initial commit

---
+++ src/test/java/hello/ApplicationSmokeTests.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationSmokeTests {
+
+	@Autowired
+	private HelloController controller;
+
+	@Test
+	public void contextLoads() throws Exception{
+		assertThat(controller).isNotNull();
+	}
+
+}
